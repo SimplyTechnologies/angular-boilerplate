fbee876811657a06c200e4ed2cfddb6f9b812535
shahen94 <shahen.hovhannisyan.94@gmail.com>
2017-01-10 00:04:39 +0400

    feat(Router): added routing example

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+
+const moduleRoutes: Routes = [
+  { path: '', component: WelcomeComponent },
+  { path: 'dashboard', component: DashboardComponent }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
+    DashboardComponent,
+    WelcomeComponent,
   ],
   imports: [
+    RouterModule.forRoot(moduleRoutes),
     BrowserModule,
     FormsModule,
     HttpModule
