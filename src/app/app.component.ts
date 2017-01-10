2655865b445d9c141539cf0347ad9f7dc98e8335
Shahen Hovhannisyan <shahen@simplytechnologies.net>
2017-01-10 12:29:20 +0400

    chore(Example): Example how to interact with backend

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { Http, Response, Headers, RequestOptions } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+import { environment } from '../environments/environment';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +11,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Welcome';
+  private http: Http;
+  constructor(_http: Http) {
+    this.http = _http;
+    this.http
+      .get(`${environment.host}/users`)
+      .map((res: Response) => res.json())
+      .subscribe(
+        (response) => console.log(response),
+        (err) => console.warn(err)
+      );
+  }
 }
